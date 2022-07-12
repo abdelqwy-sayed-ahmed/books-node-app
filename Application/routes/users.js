db0a21dae2256bbd95d7d400aa2d7301b349814f
Abdelqwy <abdelqwysayed@gmail.com>
2022-07-12 06:21:23 -0700

    update content

--- Application/routes/users.js
+++ Application/routes/users.js
@@ -1,10 +1,10 @@
-﻿'use strict';
-var express = require('express');
+﻿"use strict";
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res) {
-    res.send('respond with a resource');
+router.get("/", function (req, res) {
+  res.send("users route work well");
 });
 
 module.exports = router;
